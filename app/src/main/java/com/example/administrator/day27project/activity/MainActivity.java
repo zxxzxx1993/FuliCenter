f380599792fd3227b2acf62ca316870a2ff193ce
zxxzxx1993 <1803866319@qq.com>
2016-10-14 19:39:25 +0800

    实现onCheckedChange方法

--- app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.administrator.day27project.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.administrator.day27project.R;
 import com.example.administrator.day27project.utils.L;
@@ -11,7 +12,11 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
         L.i(" 哈哈哈哈 ");
+        setContentView(R.layout.activity_main);
+
+    }
+    public  void onCheckedChange(View view){
+
     }
 }
