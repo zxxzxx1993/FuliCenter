bac49c83cfa57c20f09f2ae2f5b4b747d7712668
zxxzxx1993 <1803866319@qq.com>
2016-10-14 15:53:45 +0800

    添加L（Log输出）工具类，并在MainActivity中测试

--- app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
@@ -1,7 +1,10 @@
-package com.example.administrator.day27project;
+package com.example.administrator.day27project.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.administrator.day27project.R;
+import com.example.administrator.day27project.utils.L;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        L.i(" 哈哈哈哈 ");
     }
 }
