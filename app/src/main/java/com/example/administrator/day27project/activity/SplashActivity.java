1398bd8e8e5fd0418f86dee58b4dd8f7d2e855af
zxxzxx1993 <1803866319@qq.com>
2016-10-14 14:58:36 +0800

    闪屏2秒，进入MainActivity

--- app/src/main/java/com/example/administrator/day27project/activity/SplashActivity.java
+++ app/src/main/java/com/example/administrator/day27project/activity/SplashActivity.java
@@ -1,16 +1,38 @@
 package com.example.administrator.day27project.activity;
 
+import android.content.Intent;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import com.example.administrator.day27project.R;
 
 public class SplashActivity extends AppCompatActivity {
-
+     private  final long Totaltime = 2000;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
     }
-    
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+             long startTime =  SystemClock.currentThreadTimeMillis();
+               long time = SystemClock.currentThreadTimeMillis();
+                long costTime = time-startTime;
+                if (Totaltime-costTime>0){
+                    try {
+                        Thread.sleep(Totaltime-costTime);
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    }
+                }
+                startActivity(new Intent(SplashActivity.this,MainActivity.class));
+            }
+        }).start();
+    }
 }
