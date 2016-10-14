7c6bcc8444c5aea0ea55e4469f7bbaa99536cb6a
zxxzxx1993 <1803866319@qq.com>
2016-10-14 14:49:24 +0800

    添加闪屏activity

---
+++ app/src/main/java/com/example/administrator/day27project/activity/SplashActivity.java
@@ -0,0 +1,16 @@
+package com.example.administrator.day27project.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.administrator.day27project.R;
+
+public class SplashActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+    }
+    
+}
