a4648c2abb85bf93fd3d264896214d77fb942b36
zxxzxx1993 <1803866319@qq.com>
2016-10-17 18:16:46 +0800

    添加网络请求的工具类以及application

---
+++ app/src/main/java/com/example/administrator/day27project/FuLiCenterApplication.java
@@ -0,0 +1,25 @@
+package com.example.administrator.day27project;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/10/17.
+ */
+public class FuLiCenterApplication extends Application{
+    public static FuLiCenterApplication application;
+    private  static FuLiCenterApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        application = this;
+        instance = this;
+    }
+
+    public  static FuLiCenterApplication getInstance(){
+        if (instance==null){
+            instance = new FuLiCenterApplication();
+        }
+        return instance;
+    }
+}
