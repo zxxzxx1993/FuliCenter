88dfea9878ea5e089d6ba159de8bc5bfa44c6cab
zxxzxx1993 <1803866319@qq.com>
2016-10-17 13:19:10 +0800

    bufferknife的安装使用，并且解决底部菜单按钮互斥问题

--- app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/day27project/activity/MainActivity.java
@@ -3,20 +3,76 @@ package com.example.administrator.day27project.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.RadioButton;
 
 import com.example.administrator.day27project.R;
 import com.example.administrator.day27project.utils.L;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
+
+public class MainActivity extends AppCompatActivity {
+    @Bind(R.id.layout_boutique)
+    RadioButton  boutique;
+    @Bind(R.id.layout_category)
+    RadioButton category;
+    @Bind(R.id.layout_new_goods)
+    RadioButton newGoods;
+    @Bind(R.id.layout_personal_center)
+    RadioButton personalCenter;
+    @Bind(R.id.layout_cart)
+    RadioButton cart;
+    RadioButton [] btns;
+    int index;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         L.i(" 哈哈哈哈 ");
         setContentView(R.layout.activity_main);
-
+        ButterKnife.bind(this);
+        initView();
     }
-    public  void onCheckedChange(View view){
 
+    private void initView() {
+      btns  = new RadioButton[5];
+        btns[0] = boutique;
+        btns[1] = category;
+        btns[2] = newGoods;
+        btns[3] = personalCenter;
+        btns[4] = cart;
+    }
+
+
+    public  void onCheckedChange(View view){
+          switch (view.getId()){
+              case R.id.layout_boutique:
+                  index=0;
+                  break;
+              case  R.id.layout_category:
+                  index=1;
+                  break;
+              case  R.id.layout_new_goods:
+                  index=2;
+                  break;
+              case R.id.layout_personal_center:
+                  index=3;
+                  break;
+              case R.id.layout_cart:
+                  index=4;
+                  break;
+          }
+        setRadioButtonStatus();
+    }
+
+    private void setRadioButtonStatus() {
+        for (int i = 0;i<btns.length;i++){
+            if (i==index){
+                btns[i].setChecked(true);
+            }
+            else{
+                btns[i].setChecked(false);
+            }
+        }
     }
 }
